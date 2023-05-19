2004a334c693ca098a5cabdf4bdbfd5b0c2d909e
diogoeduardo06 <123772268+diogoeduardo06@users.noreply.github.com>
2023-05-19 11:50:44 -0300

    exercicios python

---
+++ exercicio-1.py
@@ -0,0 +1,9 @@
+print("Ele é meu \"amigo\"") 
+
+print("10 x 10 =", 10*10)
+
+print(pow(10, 2))
+
+
+nome = str("Diogo")
+print(f"Olá, {nome} seja bem vindo")
---
+++ nome.py
@@ -0,0 +1,5 @@
+nome = str("")
+
+nome = input("Coloque seu nome: ")
+
+print(f"Olá, {nome} seja bem vindo")
