fc17fafdf45ea3ea76cfc5830e01d95adfab944c
diogoeduardo06 <123772268+diogoeduardo06@users.noreply.github.com>
2023-05-24 16:26:52 -0300

    idade.

--- Idade.py
+++
@@ -1,9 +0,0 @@
-Nascimento = int(input("Ano que nasceu: "))
-Ano = int(input("Ano atual: "))
-
-idade = (Ano - Nascimento)
-
-if idade >= 18:
-    print("Maior de idade")
-else:
-    print("Menor de idade")
